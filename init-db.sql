7126a4e74ae1852d01a50b44dea3c49c7a6fbac1
tienpham59 <pht.phamtien@gmail.com>
2024-06-13 18:44:08 +0700

    Update init-db.sql

--- init-db.sql
+++ init-db.sql
@@ -1,12 +1,12 @@
-CREATE TABLE IF NOT EXISTS tblbookIUHX (
+CREATE TABLE IF NOT EXISTS tblbookIUH1469961 (
     id serial PRIMARY KEY,
     title varchar NOT NULL,
     author varchar NOT NULL
 );
 
 INSERT INTO tblbook (title, author) VALUES
-  ('DevOps', 'IUHX'),
-  ('Big Data', 'IUHX'),
-  ('Cloud Deployement', 'IUHX'),
-('Data Analysis', 'IUHX'),
-('Block Chain', 'IUHX');
+  ('DevOps', 'IUH1469961'),
+  ('Big Data', 'IUH1469961'),
+  ('Cloud Deployement', 'IUH1469961'),
+('Data Analysis', 'IUH1469961'),
+('Block Chain', 'IUH1469961');
